f1c42f9941104021e1f56d2e8d8b8446ad0cffd6
caffibrowse <cadelacy6107@gmail.com>
2026-02-20 19:13:23 -0600

    Made "ModBlocks.java" and made it log when mod is booted.

--- src/main/java/net/pyro/tutorialmod/TutorialMod.java
+++ src/main/java/net/pyro/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.pyro.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.pyro.tutorialmod.block.ModBlocks;
 import net.pyro.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/pyro/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package net.pyro.tutorialmod.block;
+
+import net.pyro.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering mod blocks for" + TutorialMod.MOD_ID + ".");
+    }
+}
