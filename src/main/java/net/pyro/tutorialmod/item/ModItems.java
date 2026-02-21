05379d23153c8287ac8db51240ccd6f508a750e6
caffibrowse <cadelacy6107@gmail.com>
2026-02-20 19:02:49 -0600

    Items fixed

--- src/main/java/net/pyro/tutorialmod/item/ModItems.java
+++ src/main/java/net/pyro/tutorialmod/item/ModItems.java
@@ -14,7 +14,7 @@ public class ModItems{
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, "pink_garnet"), item);
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
